77fbda5a220430e200961975dca6ebd7a06b204e
kazuki8001 <ngtkzk0801@gmail.com>
2025-05-25 13:52:19 +0900

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -6,32 +6,32 @@ def lambda_handler(event, context):
 
     # 1. 入力パラメータの値をリストに格納
     required_params = ['reviewText', 'userName', 'mailAddress']
-
+    # 2. 未入力パラメータをリストに格納(リストが空の場合全て入力されている)
     param_empty_check = [
         param for param in required_params 
         if param not in event or not str(event[param]).strip()
         ]
 
-    # 2. 入力パラメータのチェック
+    # 3. 入力パラメータのチェック
     if param_empty_check:
         return {
             'statusCode': 400,
             'body': json.dumps('param does not exist')
         }
     
-    # 3.入力パラメータの取得
+    # 4.入力パラメータの取得
     reviewText = event["reviewText"]  # 問い合わせの内容
     userName = event["userName"]  # 問い合わせの投稿者名
     mailAddress = event["mailAddress"]  # 問い投稿者のメールアドレス
 
-    # 4.idの生成（uuidを取得）
+    # 5.idの生成（uuidを取得）
     item_id = str(uuid.uuid4()) 
     
-    # 5.DynamoDBリソースの初期化
+    # 6.DynamoDBリソースの初期化
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table('lnquiryTable')
     
-    # 6.DynamoDBに更新するitemの内容を辞書で定義
+    # 7.DynamoDBに更新するitemの内容を辞書で定義
     item = {
         'id': item_id,
         'reviewText': reviewText,
@@ -40,16 +40,16 @@ def lambda_handler(event, context):
     }
     
     try:
-        # 7.DynamoDBにデータを保存
+        # 8.DynamoDBにデータを保存
         table.put_item(Item=item)
     except Exception as e:
-        # 8.エラーが発生した場合、ステータスコード500（内部サーバエラー）を返す
+        # 9.エラーが発生した場合、ステータスコード500（内部サーバエラー）を返す
         return {
             'statusCode': 500,
             'body': json.dumps(f'Error saving item to DynamoDB: {str(e)}')
         }
     
-    # 9.ステータスコード200（正常終了）を返す
+    # 10ステータスコード200（正常終了）を返す
     return {
         'statusCode': 200,
         'body': json.dumps({
