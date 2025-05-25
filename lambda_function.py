87dec537b3fed17178451dadd0b54af9f3c70717
kazuki <ngtkzk0801@gmail.com>
2025-05-25 11:03:45 +0900

    初回コミット

---
+++ lambda_function.py
@@ -0,0 +1,59 @@
+import json
+import boto3
+import uuid
+
+def lambda_handler(event, context):
+
+    # 1. 入力パラメータの値をリストに格納
+    required_params = ['reviewText', 'userName', 'mailAddress']
+
+    param_empty_check = [
+        param for param in required_params 
+        if param not in event or not str(event[param]).strip()
+        ]
+
+    # 2. 入力パラメータのチェック
+    if param_empty_check:
+        return {
+            'statusCode': 400,
+            'body': json.dumps('param does not exist')
+        }
+    
+    # 3.入力パラメータの取得
+    reviewText = event["reviewText"]  # 問い合わせの内容
+    userName = event["userName"]  # 問い合わせの投稿者名
+    mailAddress = event["mailAddress"]  # 問い投稿者のメールアドレス
+
+    # 4.idの生成（uuidを取得）
+    item_id = str(uuid.uuid4()) 
+    
+    # 5.DynamoDBリソースの初期化
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('lnquiryTable')
+    
+    # 6.DynamoDBに更新するitemの内容を辞書で定義
+    item = {
+        'id': item_id,
+        'reviewText': reviewText,
+        'userName': userName,
+        'mailAddress': mailAddress
+    }
+    
+    try:
+        # 7.DynamoDBにデータを保存
+        table.put_item(Item=item)
+    except Exception as e:
+        # 8.エラーが発生した場合、ステータスコード500（内部サーバエラー）を返す
+        return {
+            'statusCode': 500,
+            'body': json.dumps(f'Error saving item to DynamoDB: {str(e)}')
+        }
+    
+    # 9.ステータスコード200（正常終了）を返す
+    return {
+        'statusCode': 200,
+        'body': json.dumps({
+            'message': 'lnquiryTable saved successfully!',
+            'id': item_id
+        })
+    }
